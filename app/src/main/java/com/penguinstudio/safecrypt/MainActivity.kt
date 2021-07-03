895aa2d488fae119b52d6803729e3e070803b69b
BoostedPenguin <60142028+BoostedPenguin@users.noreply.github.com>
2021-07-03 13:59:47 +0300

    Clean start, removed boilerplate

--- app/src/main/java/com/penguinstudio/safecrypt/MainActivity.kt
+++ app/src/main/java/com/penguinstudio/safecrypt/MainActivity.kt
@@ -1,14 +1,7 @@
 package com.penguinstudio.safecrypt
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import com.penguinstudio.safecrypt.ui.main.SectionsPagerAdapter
 import com.penguinstudio.safecrypt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,20 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = binding.viewPager
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = binding.tabs
-        tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = binding.fab
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 }
